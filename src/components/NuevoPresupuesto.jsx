aac43c22cd92c87974f425b45f37a32c603d578b
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-03-05 23:21:51 -0600

    Validating the budget and Message component

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState("")
+
+    const handlePresupuesto = (e)=>{
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje("No es un presupuesto valido")
+        }else{
+            setMensaje("Si es un presupuesto válido")
+        }
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario' action="">
+        <form onSubmit={handlePresupuesto} className='formulario' action="">
             <div className='campo'>
                 <label htmlFor="">Definir Presupuesto</label>
                 <input 
                 type="text"
                 className='nuevo-presupuesto'
                 placeholder='Añade tu Presupuesto'
+                value={presupuesto}
+                onChange = {(e)=>setPresupuesto(e.target.value)}
                 />
             </div>
 
             <input type="submit" value="Añadir"/>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje> }
         </form>
     </div>
   )
