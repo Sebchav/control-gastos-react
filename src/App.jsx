827c11d262c88c7c011fe25223e5dc2aa0fa14d7
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-03-06 20:35:26 -0600

    New expense and Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from "./img/nuevo-gasto.svg"
+import Modal from './components/Modal';
+
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = ()=>{
+    setModal(true)
+  }
 
   return (
     <div>
       <Header
       presupuesto = {presupuesto}
-      setPresupuesto = {setPresupuesto}   
+      setPresupuesto = {setPresupuesto}
+      isValidPresupuesto = {isValidPresupuesto}
+      setIsValidPresupuesto = {setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+          <div className='nuevo-gasto'>
+            <img 
+              src={IconoNuevoGasto}
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+      )}
+
+      {modal && <Modal setModal={setModal} />}
+      
     </div>
   )
 }
