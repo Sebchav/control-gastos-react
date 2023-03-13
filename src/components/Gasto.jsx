ff5bc2c8b1e5cde8d4be8f3dd8bce756bf1c5b0e
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-03-12 20:13:49 -0600

    Budget spent and available

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,11 +1,33 @@
 
 import { formatearFecha } from "../helpers";
+
+import iconoAhorro from "../img/icono_ahorro.svg"
+import iconoCasa from "../img/icono_casa.svg"
+import iconoComida from "../img/icono_comida.svg"
+import iconoGastos from "../img/icono_gastos.svg"
+import iconoOcio from "../img/icono_ocio.svg"
+import iconoSalud from "../img/icono_salud.svg"
+import iconoSuscripciones from "../img/icono_suscripciones.svg"
+
+const diccionarioIconos = {
+  ahorro : iconoAhorro,
+  comida : iconoComida,
+  casa : iconoCasa,
+  gastos : iconoGastos,
+  ocio : iconoOcio,
+  salud : iconoSalud,
+  suscripciones : iconoSuscripciones
+}
+
 const Gasto = ({gasto}) => {
     const {categoria, nombre, cantidad, id, fecha} = gasto;
   return (
     <div className="gasto sombra">
         <div className="contenido-gasto">
-            {/* IMAGEN */}
+            <img 
+              src={diccionarioIconos[categoria]}
+              alt="Icono Gasto" 
+            />
             <div className="descripcion-gasto">
                 <p className="categoria">{categoria}</p>
                 <p className="nombre-gasto">{nombre}</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
+const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto, gastos}) => {
   return (
     <header>
         <h1>Planificador de Gastos</h1>
@@ -10,6 +10,7 @@ const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPres
         {isValidPresupuesto ? (
           <ControlPresupuesto 
             presupuesto = {presupuesto}
+            gastos = {gastos}
           />
         ) : (
           <NuevoPresupuesto
