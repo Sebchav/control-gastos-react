2bd9aa9673280e6ec4122389fe59293d97ff8aa6
Chava <schavarria13@alumnos.uaq.mx>
2023-03-17 11:49:16 -0600

    LocalStorage and Reset App... Project 7 finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto, gastos}) => {
+const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto, gastos, setGastos}) => {
   return (
     <header>
         <h1>Planificador de Gastos</h1>
 
         {isValidPresupuesto ? (
           <ControlPresupuesto 
-            presupuesto = {presupuesto}
             gastos = {gastos}
+            setGastos = {setGastos}
+            presupuesto = {presupuesto}
+            setPresupuesto = {setPresupuesto}
+            setIsValidPresupuesto = {setIsValidPresupuesto}
           />
         ) : (
           <NuevoPresupuesto
